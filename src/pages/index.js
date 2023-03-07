dc545dd9892025866a49b50acdbd4a68f8643567
hey-sk <stephenakeane@gmail.com>
2023-03-06 20:22:46 -0500

    oops-- fixed issue with api key!

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,9 @@ import styles from '@/styles/Home.module.css'
 import Weather from '@/components/Weather'
 
 export async function getStaticProps() {
-  const locRes = await fetch(`http://api.openweathermap.org/geo/1.0/zip?zip=01453,US&appid=7269f44cd91ddd4c6f9b13ca6612cc6a`)
+  const locRes = await fetch(`http://api.openweathermap.org/geo/1.0/zip?zip=01453,US&appid=c71fff6ff8aaa6bfef85d0cf02d3ef5e`)
   const locationData = await locRes.json()
-  const weatherRes = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${locationData.lat}&lon=${locationData.lon}&units=imperial&appid=7269f44cd91ddd4c6f9b13ca6612cc6a`)
+  const weatherRes = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${locationData.lat}&lon=${locationData.lon}&units=imperial&appid=${process.env.OPENWEATHER}`)
   const weatherData = await weatherRes.json()
   return {
     props: {
